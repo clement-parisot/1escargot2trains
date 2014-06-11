5ebceb6b83efd86113f6fa28ba0ae388ba8f9334
clement-parisot <clement.parisot@telecomnancy.net>
2014-06-11 00:24:00 -0400

    empty project

---
+++ desktop/src/com/escargot/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.escargot.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.escargot.game.EscargotGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EscargotGame(), config);
+	}
+}
